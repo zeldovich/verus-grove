a11c38a5113fa8b7da5624c1f3371e179e2dd25f
Upamanyu Sharma <upamanyu@mit.edu>
2023-11-15 13:58:31 -0500

    Tricky to use logical atomicity bc of some non-fundamental Verus limitations

--- logical_atomicity.rs
+++ logical_atomicity.rs
@@ -1,11 +1,8 @@
 use vstd::{prelude::*, invariant::*};
-mod kv;
-use kv::lmap;
 
 verus! {
 
-    pub struct KvState<P, C, Pred> {
-        m: lmap::LMap<u64, u64>,
+    pub struct KvClient<P, C, Pred> {
         _p:std::marker::PhantomData<P>,
         _c:std::marker::PhantomData<C>,
         _pred:std::marker::PhantomData<Pred>,
@@ -25,7 +22,7 @@ verus! {
     }
 
     // Q: put traits only in `impl` or also on struct?
-    impl<P, C, Pred:InvariantPredicate<(P,Map<u64,u64>), C,>> KvState<P,C,Pred> {
+    impl<P, C, Pred:InvariantPredicate<C, (P,Map<u64,u64>)>> KvClient<P,C,Pred> {
         pub spec fn constant(self) -> C;
 
         pub closed spec fn inv(self) -> bool {
@@ -43,63 +40,145 @@ verus! {
 
         #[verifier(external_body)]
         proof fn example_fupd2(args:(Ghost<Map<u64,u64>>, Tracked<P>), c:C) -> (ret:P)
-            requires Pred::inv((args.1@, args.0@), c)
-            ensures Pred::inv((ret, args.0@.insert(2u64,37u64)), c)
+            requires Pred::inv(c, (args.1@, args.0@))
+            ensures Pred::inv(c, (ret, args.0@.insert(2u64,37u64)))
             opens_invariants any
         {
             todo!();
         }
 
+        // TODO: predicate-wrap Phi?
         // requires [ ∀ σ, P σ ==∗ P (σ.insert(k,v)) ∗ Φ ]
         // ensures  Φ
         #[verifier(external_body)]
         pub fn put_hocap<Phi, F: FnOnce(Tracked<P>, Ghost<Map<u64,u64>>) -> (Tracked<P>, Phi) >
-            (&mut self, k:u64, v:u64, au:Tracked<F>) -> (ret:Phi)
+            (&self, k:u64, v:u64, au:Tracked<F>) -> (ret:Phi)
 
             requires
-            old(self).inv(),
+            self.inv(),
             // XXX: can't do it this way
             // forall |sigma, res| Pred::inv((res, sigma), old(self).constant()) ==> au@.requires((Tracked(res), Ghost(sigma))),
             forall |sigma:Ghost<_>, res:Tracked<_>, res_prime:Tracked<_>, phi|
-            (#[trigger] Pred::inv((res@, sigma@), old(self).constant()) ==> au@.requires((res, sigma))) &&
-            (#[trigger] au@.ensures((res, sigma), (res_prime, phi)) ==> Pred::inv((res_prime@, sigma@.insert(k,v)), old(self).constant())),
-
-            ensures
-            self.inv(),
-            self.constant() == old(self).constant(),
+            (#[trigger] Pred::inv(self.constant(), (res@, sigma@)) ==> au@.requires((res, sigma))) &&
+            (#[trigger] au@.ensures((res, sigma), (res_prime, phi)) ==> Pred::inv(self.constant(), (res_prime@, sigma@.insert(k,v)))),
         {
             unimplemented!();
         }
 
         // TODO: need to have a FnSpec or Trait predicate for Φ as well
-        // requires [ ∀ σ, P σ ==∗ P (σ.insert(k,v)) ∗ Φ(lookup(σ,k) == exp) ]
+        // requires [ ∀ σ, P σ ==∗ P σ' ∗ Φ(lookup(σ,k) == exp) ]
         // ensures  Φ
         #[verifier(external_body)]
-        pub fn cput_hocap<Phi, F: FnOnce(Tracked<P>, Ghost<Map<u64,u64>>) -> (Tracked<P>, Phi) >
-            (&mut self, k:u64, exp:u64, v:u64, au:Tracked<F>) -> (ret:(bool, Phi))
+        pub fn cput_hocap<PhiRes, F: FnOnce(Tracked<P>, Ghost<Map<u64,u64>>) -> (Tracked<P>, PhiRes) >
+            (&self, k:u64, exp:u64, v:u64, au:F,
+             phiPred: (FnSpec(bool, PhiRes) ->  bool), // XXX: curry?
+            ) -> (ret:(bool, PhiRes))
 
             requires
-            old(self).inv(),
-            forall |sigma:Ghost<_>, res:Tracked<_>, res_prime:Tracked<_>, phi|
-            (#[trigger] Pred::inv((res@, sigma@), old(self).constant()) ==> au@.requires((res, sigma))) &&
-            (#[trigger] au@.ensures((res, sigma), (res_prime, phi)) ==> Pred::inv((res_prime@, sigma@.insert(k,v)), old(self).constant())),
+            self.inv(),
+                forall |sigma:Ghost<_>, res:Tracked<_>, res_prime:Tracked<_>, phi|
+                (#[trigger] Pred::inv(self.constant(), (res@, sigma@)) ==> au.requires((res, sigma))) &&
+                (#[trigger] au.ensures((res, sigma), (res_prime, phi)) ==> Pred::inv(self.constant(), (res_prime@, sigma@.insert(k,v)))),
 
             ensures
-            self.inv(),
-            self.constant() == old(self).constant(),
         {
             unimplemented!();
         }
 
         // requires (P ∅)
         #[verifier(external_body)]
-        pub fn new(res:P, c:C) -> (kv:KvState<P,C,Pred>)
-            requires Pred::inv((res, Map::<u64,u64>::empty()), c),
+        pub fn new(res:P, c:C) -> (kv:KvClient<P,C,Pred>)
+            requires Pred::inv(c, (res, Map::<u64,u64>::empty())),
 
             ensures kv.constant() == c,
         {
             unimplemented!();
         }
+    }
+
+    enum Or<A,B> {
+        Left(A),
+        Right(B),
+    }
+
+    type LockInvRes<R> = Or<R,()>;
+    struct LockInvConsts {}
+    struct LockInvPredicate{}
+
+    // InvariantPredicate<(P,Map<u64,u64>)
+    impl<R> InvariantPredicate<LockInvConsts, (LockInvRes<R>, Map<u64,u64>)> for LockInvPredicate {
+        closed spec fn inv(c: LockInvConsts, r: (LockInvRes<R>, Map<u64,u64>)) -> bool {
+            if lookup(r.1, 37) == 0 {
+                match r.0 {
+                    Or::Left(_) => true,
+                    Or::Right(_) => false,
+                }
+            } else {
+                true
+            }
+        }
+    }
+
+    type PhiRes<R> = Or<R,()>;
+
+    // phiPred: (FnSpec(bool, PhiRes) ->  bool)
+    spec fn phi_pred<R>() -> FnSpec(bool, PhiRes<R>) -> bool {
+        |ret:bool, res:PhiRes<R>| {
+            true
+        }
+    }
+
+    fn trylock_cput_au<R>(res:Tracked<LockInvRes<R>>, sigma:Ghost<Map<u64,u64>>) ->
+        (Tracked<LockInvRes<R>>, PhiRes<R>)
+    {
+        match res@ {
+            Or::Left(r) => {
+                return (Tracked(Or::Right(())), Or::Left(r));
+            }
+            Or::Right(()) => {
+                return (Tracked(Or::Right(())), Or::Right(()));
+            }
+        }
+    }
+
+    fn spinlock_acquire<R>(kv:&KvClient<LockInvRes<R>, LockInvConsts, LockInvPredicate>)
+        -> Tracked<R>
+    {
+       loop {
+           // F: FnOnce(Tracked<P>, Ghost<Map<u64,u64>>) -> (Tracked<P>, PhiRes) >
+           // (&self, k:u64, exp:u64, v:u64, au:Tracked<F>, phiPred: (FnSpec(bool, PhiRes) ->  bool), ) -> (ret:(bool, PhiRes))
+           // let au = |res:Tracked<_>, sigma| -> (Tracked<LockInvRes<R>>, PhiRes<R>)
+           //     // FIXME: requires/ensures
+           // {
+           //     match res@ {
+           //         Or::Left(r) => {
+           //             return (Tracked(Or::Right(())), Or::Left(r));
+           //         }
+           //         Or::Right(()) => {
+           //             return (Tracked(Or::Right(())), Or::Right(()));
+           //         }
+           //     }
+           // };
+
+           // let phi_pred= |ret, res| {
+           //    true
+           // };
+           let ret = kv.cput_hocap(37, 0, 1, trylock_cput_au, phi_pred());
+           if ret.0 == true {
+               let tracked r;
+               proof {
+                   match ret.1 {
+                       Or::Left(r2) => {
+                           r = r2;
+                       }
+                       Or::Right(()) => {
+                           assert(false);
+                       }
+                   }
+               }
+               return Tracked(r)
+           }
+       }
     }
 
     fn main() {}
