fe28020d18850dd3160abeba46f0e19831f848b8
Upamanyu Sharma <upamanyu@mit.edu>
2023-11-20 13:17:38 -0500

    Checkpoint helping example

---
+++ helping.rs
@@ -0,0 +1,86 @@
+use vstd::{prelude::*,invariant::*,thread::*};
+mod lock;
+mod kv;
+use kv::*;
+
+verus! {
+
+    // possible `state`s
+    const UNUSED: u64 = 0;
+    const ACTIVE: u64 = 1;
+    const DONE: u64 = 2;
+
+    struct WriteRequest {
+        val:u64,
+        state:u64, // see above for possible values
+    }
+
+    // sort-of a "flat combining" register, but with no lock-free accesses and a
+    // publication list of length 1.
+    pub struct Register {
+        plist: lock::Lock<WriteRequest>,
+        val: lock::Lock<u64>,
+    }
+
+    impl Register {
+        spec fn inv(self) -> bool {
+            true
+        }
+
+        fn combiner(&self)
+            requires self.inv()
+        {
+            // repeatedly
+            loop {
+                let req = self.plist.lock();
+                if req.state == ACTIVE {
+                    let _ = self.val.lock();
+                    self.val.unlock(req.val);
+                }
+                self.plist.unlock(req);
+            }
+        }
+
+        fn read(&self) -> u64
+            requires self.inv()
+        {
+            let v = self.val.lock();
+            self.val.unlock(v);
+            return v;
+        }
+
+        fn write(&self, v:u64)
+            requires self.inv()
+        {
+            let mut req;
+            // wait for publication list to have a unused slot
+            loop {
+                req = self.plist.lock();
+                if req.state == UNUSED {
+                    break
+                }
+                self.plist.unlock(req);
+            }
+
+            // send request
+            req.state = ACTIVE;
+            req.val = v;
+            self.plist.unlock(req);
+
+            // wait for response (of type unit)
+            loop {
+                req = self.plist.lock();
+                if req.state == DONE {
+                    break
+                }
+                self.plist.unlock(req);
+            }
+
+            req.state = UNUSED;
+            self.plist.unlock(req);
+        }
+    }
+
+    fn main() {
+    }
+} // verus!
