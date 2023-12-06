ec6306182635a0d49801113a933190e3f79b6b46
Upamanyu Sharma <upamanyu@mit.edu>
2023-12-06 17:37:47 -0500

    Wrapper around some of rust std::net, enough to run echo server

--- echo.rs
+++ echo.rs
@@ -1,9 +1,9 @@
 use vstd::{prelude::*,invariant::*,thread::*};
-use std::net::{TcpListener, TcpStream, ToSocketAddrs};
-use std::io::{Read,Write,Result};
+use std::net::{TcpListener, TcpStream};
+use std::io::{Read,Write};
+use std::string::String;
 
 verus! {
-
     // Ghost resources for network
     #[verifier(external_body)]
     pub tracked struct NetworkPointsTo {
@@ -14,112 +14,132 @@ verus! {
         spec fn msgs() -> Set<Seq<u8>>;
     }
 
+    // NOTE: ideally, we would give specs to the build std::net functions. There
+    // are two problems:
+    // (1) want to add ghost resources in pre/post conditions of these functions
+    // (2) we don't want to use TCP or UDP, we want a messaging layer that does
+    //     not guarantee ordering of messages but also does not limit message
+    //     size, so don't really want to use either UdpSocket or TcpStream.
+
     // Types for network
+    #[verifier(external_body)]
+    pub struct Connection {
+        mu:std::sync::Mutex<TcpStream>
+    }
+
+    #[verifier(external_body)]
+    pub struct Listener {
+        l:TcpListener
+    }
+
+    #[verifier(external_type_specification)]
+    #[verifier(external_body)]
+    pub struct ExString(String);
+
     #[verifier(external_type_specification)]
     #[verifier(external_body)]
     pub struct ExError(std::io::Error);
 
-    #[verifier(external_type_specification)]
-    #[verifier(external_body)]
-    pub struct ExTcpStream(TcpStream);
+    impl Listener {
+        #[verifier(external_body)]
+        pub fn listen(addr:String) -> Result<Self, std::io::Error> {
+            Ok(Listener{l: TcpListener::bind(addr)?})
+        }
 
+        #[verifier(external_body)]
+        pub fn accept(&self) -> Connection {
+            loop {
+                match self.l.accept() {
+                    Ok((stream, _addr)) => {
+                        return Connection{mu: std::sync::Mutex::new(stream)}
+                    }
+                    Err(e) => continue
+                }
+            }
+        }
+    }
 
-    // FIXME: will need to add ownership-related pre+post conditions to these
-    // functions, which means external_fn_specification likely won't work.
+    impl Connection {
+        #[verifier(external_body)]
+        pub fn connect(addr:String) -> Result<Self, std::io::Error> {
+            let stream = TcpStream::connect(addr)?;
+            Ok(Self{mu: std::sync::Mutex::new(stream)})
+        }
 
-    // Hoare triples for network
-    #[verifier::external_fn_specification]
-    pub fn ex_connect<A: ToSocketAddrs>(addr: A) -> Result<TcpStream> {
-        TcpStream::connect(addr)
+        #[verifier(external_body)]
+        pub fn receive(&self) -> Vec<u8> {
+            let mut sz_enc = [0u8; 8];
+            let mut s = self.mu.lock().unwrap();
+            s.read_exact(&mut sz_enc).unwrap();
+            let sz = u64::from_ne_bytes(sz_enc);
+            let mut msg = vec![0; sz as usize];
+            s.read_exact(&mut msg).unwrap();
+            msg
+        }
+
+        #[verifier(external_body)]
+        pub fn send(&self, msg:&[u8]) {
+            let mut sz_enc = msg.len().to_ne_bytes();
+            let mut s = self.mu.lock().unwrap();
+            s.write(&sz_enc).unwrap();
+            s.write(msg).unwrap();
+        }
     }
 
     // Hoare triples for network
 
-    // XXX: why are there impls `Write for &TcpStream` and `Write for TcpStream` both?
-    #[verifier::external_fn_specification]
-    pub fn ex_write(sf:&mut TcpStream, buf: &[u8]) -> Result<usize> {
-       sf.write(buf)
-    }
+    fn client_main(addr:String) {
+        #[cfg(not(verus_keep_ghost_body))]
+        println!("Running client");
 
-    #[verifier::external_fn_specification]
-    pub fn ex_read(sf:&mut TcpStream, buf: &mut [u8]) -> Result<usize> {
-       sf.read(buf)
-    }
-
-    #[verifier::external_fn_specification]
-    pub fn ex_read_to_end(sf:&mut TcpStream, buf: &mut Vec<u8>) -> Result<usize> {
-       sf.read_to_end(buf)
-    }
-
-    /*
-    // FIXME: can't get this to work.
-    #[verifier::external_fn_specification]
-    pub fn ex_write2<'b>(sf:&mut &TcpStream, buf: &[u8]) -> Result<usize> {
-       sf.write(buf)
-    }
-    */
-    fn vec_test() {
-        let mut x : std::vec::Vec<u8> = vec![];
+        let mut maybe_conn = Connection::connect(addr);
+        assume(maybe_conn.is_Ok());
+        let conn = maybe_conn.unwrap();
+        let mut x : Vec<u8> = Vec::new();
         x.push(0);
         x.push(1);
         x.push(2);
         x.push(3);
-        let y = x.as_slice();
+        conn.send(x.as_slice());
+        let y = conn.receive();
+        assert(x@ == y@);
+
+        #[cfg(not(verus_keep_ghost_body))]
+        {
+            assert_eq!(x, &y[..]);
+            println!("Received successful echo");
+        }
     }
 
-    fn client_main2(stream:&mut TcpStream) {
-        // println!("Running client");
-        let mut xv = vec![];
-        xv.push(0);
-        xv.push(1);
-        xv.push(2);
-        xv.push(3);
-
-        let x : &[u8] = xv.as_slice();
-        stream.write(x);
-        let mut y = vec![];
-        stream.read_to_end(&mut y);
-        // assert_eq!(x, y.as_slice());
-        // println!("Received successful echo");
-    }
-
-    #[verifier(external_body)]
-    fn client_main() {
-        // println!("Running client");
-        let mut stream = TcpStream::connect("127.0.0.1:12345").unwrap();
-        let x = &[0,1,2,3];
-        stream.write(x);
-        let mut y = vec![];
-        stream.read_to_end(&mut y);
-        assert_eq!(x, &y[..]);
-        // println!("Received successful echo");
-    }
-
-    #[verifier(external_body)]
-    fn server_main() {
+    fn server_main(addr:String) {
+        #[cfg(not(verus_keep_ghost_body))]
         println!("Running server");
-        let listener = TcpListener::bind("127.0.0.1:12345").unwrap();
 
-        // could also call accept(), which the incoming Iterator wraps around.
-        // https://doc.rust-lang.org/std/net/struct.TcpListener.html#method.accept
-        for stream in listener.incoming() {
-            let mut s = stream.unwrap();
-            let mut x = [0; 128];
-            let n = s.read(&mut x).unwrap();
+        let maybe_listener = Listener::listen(addr);
+        assume(maybe_listener.is_Ok());
+        let listener = maybe_listener.unwrap();
+
+        loop {
+            let conn = listener.accept();
+            let msg = conn.receive();
+
+            #[cfg(not(verus_keep_ghost_body))]
             println!("Echoing...");
-            s.write(&x[..n]);
+
+            conn.send(msg.as_slice());
         }
     }
 
     #[verifier(external_body)]
     fn main() {
+        let addr = "127.0.0.1:12345".to_string();
         for argument in std::env::args() {
             match argument.as_str() {
                 "client" => {
-                    return client_main();
+                    return client_main(addr);
                 }
                 "server" => {
-                    return server_main();
+                    return server_main(addr);
                 }
                 _ => {}
             }
